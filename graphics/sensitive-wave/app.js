01832f8ef31a4bf6b0363617d09c09e49c759bb3
Choe Simin <choesimin961009@gmail.com>
2024-01-20 18:27:14 +0900

    UPdate

---
+++ graphics/sensitive-wave/app.js
@@ -0,0 +1,67 @@
+class App {
+    constructor() {
+        const initialSpeed = 0.005;
+
+        this.speed = initialSpeed;
+        this.canvas = document.createElement('canvas');
+        this.ctx = this.canvas.getContext('2d');
+
+        document.body.appendChild(this.canvas);
+
+        this.waveGroup = new WaveGroup(this.speed);
+
+        window.addEventListener('resize', this.resize.bind(this), false);
+        this.resize();
+
+        requestAnimationFrame(this.animate.bind(this));
+
+
+        let interval = null;
+        document.addEventListener('contextmenu', (event) => event.preventDefault());
+        document.addEventListener('mousedown', () => {
+            interval = setInterval(() => {
+                this.speed = this.speed + 0.01;
+                this.setSpeed(this.speed);
+            }, 100);
+        });
+        document.addEventListener('touchstart', () => {
+            interval = setInterval(() => {
+                this.speed = this.speed + 0.01;
+                this.setSpeed(this.speed);
+            }, 100);
+        });
+        document.addEventListener('mouseup', () => {
+            this.speed = initialSpeed;
+            this.setSpeed(this.speed);
+            clearInterval(interval);
+        });
+        document.addEventListener('touchend', () => {
+            this.speed = initialSpeed;
+            this.setSpeed(this.speed);
+            clearInterval(interval);
+        });
+    }
+
+    resize() {
+        this.stageWidth = document.body.clientWidth;
+        this.stageHeight = document.body.clientHeight;
+
+        this.canvas.width = this.stageWidth * 2;
+        this.canvas.height = this.stageHeight * 2;
+        this.ctx.scale(2, 2);
+
+        this.waveGroup.resize(this.stageWidth, this.stageHeight);
+    }
+
+    animate(t) {
+        this.ctx.clearRect(0, 0, this.stageWidth, this.stageHeight);
+
+        this.waveGroup.draw(this.ctx);
+
+        requestAnimationFrame(this.animate.bind(this));
+    }
+
+    setSpeed(speed) {
+        this.waveGroup.setSpeed(speed);
+    }
+}
---
+++ graphics/sensitive-wave/point.js
@@ -0,0 +1,19 @@
+class Point {
+    constructor(index, x, y, speed) {
+        this.x = x;
+        this.y = y;
+        this.fixedY = y;
+        this.speed = speed;
+        this.cur = index;
+        this.max = Math.random() * 100 + 250;
+    }
+
+    update() {
+        this.cur += this.speed;
+        this.y = this.fixedY + (Math.sin(this.cur) * this.max);
+    }
+
+    setSpeed(speed) {
+        this.speed = speed;
+    }
+}
